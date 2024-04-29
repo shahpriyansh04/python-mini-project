5226f6506acc2450bff33e24e925d78657872ae3
Priyansh Shah <shah.priyansh04@gmail.com>
2024-04-29 21:45:16 +0530

    2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 <button className='mt-12 bg-white px-12 py-4' onClick={handleClick}>Search</button>
 {
   movies.length > 0 ? (
-    <div className='grid grid-cols-4 gap-12 mt-12'>
+    <div className='grid h-full grid-cols-6 gap-12 mt-12'>
     {movies.map((movie) => (
       <div className="h-72 text-white flex flex-col items-center ">
         <img src={movie.poster} alt={movie.name} className='w-full h-full object-contain' />
